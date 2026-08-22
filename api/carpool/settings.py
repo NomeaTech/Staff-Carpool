18825566c3c5a467512881d1c1e51efbfbe457ca
bfentor <fentorb24@gmail.com>
2026-08-22 15:11:50 +0200

    add CORS_ALLOWED_ORIGINS

--- api/carpool/settings.py
+++ api/carpool/settings.py
@@ -47,13 +47,20 @@ CSRF_COOKIE_DOMAIN = os.getenv("CSRF_COOKIE_DOMAIN")
 
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = False
 
 CORS_ORIGIN_WHITELIST = (
-  'http://localhost:8120',
-  'https://ride.fentorweb.hu',
-  'https://static.cloudflareinsights.com',
+    'http://localhost:8120',
+    'https://ride.fentorweb.hu',
+    'https://static.cloudflareinsights.com',
 )
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8120',
+    'https://ride.fentorweb.hu',
+    'https://static.cloudflareinsights.com',
+]
+
 
 # Application definition
 
